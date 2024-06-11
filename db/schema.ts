3e9b3a92ef3ddee2645f8f0fc79355de27f5dfd7
MJonny4 <ion2000m@gmail.com>
2024-06-11 02:58:49 +0200

    chore: Add database migration scripts and schema for 'accounts' table

---
+++ db/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, text } from 'drizzle-orm/pg-core'
+
+export const accounts = pgTable('accounts', {
+    id: text('id').primaryKey(),
+    plaidId: text('plaid_id').notNull(),
+    name: text('name').notNull(),
+    userId: text('user_id').notNull(),
+})
