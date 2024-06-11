f5878436cf75b2ec63a884ee1309bb6a80d9a414
MJonny4 <ion2000m@gmail.com>
2024-06-11 23:50:54 +0200

    chore: Add 'transactions' route to API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,13 +3,14 @@ import { handle } from 'hono/vercel'
 
 import accounts from './accounts'
 import categories from './categories'
+import transactions from './transactions'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
 // err = Error, ctx = Context
-const routes = app.route('accounts', accounts).route('categories', categories)
+const routes = app.route('accounts', accounts).route('categories', categories).route('transactions', transactions)
 
 export const GET = handle(app)
 export const POST = handle(app)
